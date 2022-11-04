72c4ea264c16390096b5d7bbcb9a9f756ce7a107
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2022-11-04 23:25:58 +0100

    correction9

--- src/main/java/tn/esprit/rh/achat/repositories/ProduitRepository.java
+++ src/main/java/tn/esprit/rh/achat/repositories/ProduitRepository.java
@@ -1,8 +1,12 @@
 package tn.esprit.rh.achat.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tn.esprit.rh.achat.entities.Produit;
+
+import java.util.Date;
 
 @Repository
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
@@ -18,8 +22,5 @@ public interface ProduitRepository extends JpaRepository<Produit, Long> {
 	
 	
 	
-	/*@Query("SELECT sum(df.prixTotal) FROM DetailFacture df where df.produit=:produit and df.facture.dateFacture between :startDate"
-			+ " and :endDate and df.facture.active=true")
-	public float getRevenuBrutProduit(@Param("produit") Produit produit, @Param("startDate") Date startDate,
-			@Param("endDate") Date endDate);*/
+
 }
